63589609aea71388f6c2450eb957cf6a7ed7c16c
albert <alberto.ramirez.m77@gmail.com>
2024-08-16 13:40:09 +0200

    + Añado librerias a usar en bundleConfig en capaAdmin

--- CapaPresentacionAdmin/App_Start/BundleConfig.cs
+++ CapaPresentacionAdmin/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace CapaPresentacionAdmin
             bundles.Add(new Bundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new Bundle("~/bundles/complementos").Include(
+                        "~/Scripts/fontawesome/all.min.js",
+                        "~/Scripts/scripts.js"));
+
             //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
             //            "~/Scripts/jquery.validate*"));
 
@@ -19,8 +23,7 @@ namespace CapaPresentacionAdmin
             //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
             //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.bundle.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.bundle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
         }
